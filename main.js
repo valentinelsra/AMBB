749454268980978ef1801e2abba6099387ee8a73
Valentine Lesourd-Aubert <56268769+valentinelsra@users.noreply.github.com>
2020-07-26 20:46:44 +0200

    Update main.js
    MAJ :
    - Formulaire de parrainage,
    - Formulaire d'indices.
    Les modifications ne concernent que le visuel de l'application, les formulaires ne sont pas fonctionnels (aucune information ne sera stockée à cette étape du projet).

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ Previous_Button.style.display = "none";
 
 Intro_Block = document.getElementById("intro");
 Info_Block = document.getElementById("info");
-Cb_Block = document.getElementById("cb");
-Indice1_Block = document.getElementById("indice1");
-Indice2_Block = document.getElementById("indice2");
+Indices_Block = document.getElementById("indices");
 Players_Block = document.getElementById("players");
 
 function Previous_Action(){
@@ -14,9 +12,7 @@ function Previous_Action(){
     let Previous_Button = document.getElementById("previous");
     let Next_Button = document.getElementById("next");
     let Info_Block = document.getElementById("info");
-    let Cb_Block = document.getElementById("cb");
-    let Indice1_Block = document.getElementById("indice1");
-    let Indice2_Block = document.getElementById("indice2");
+    let Indices_Block = document.getElementById("indices");
     let Players_Block = document.getElementById("players");
 
 
@@ -24,26 +20,17 @@ function Previous_Action(){
         Previous_Button.style.display = "none";
         Info_Block.style.display = "none";
         Intro_Block.style.display = "block";
+        Next_Button.textContent = "Commencer l'inscription !";
     }
 
-    else if(Cb_Block.clientWidth > 0){
-        Cb_Block.style.display = "none";
+    else if(Indices_Block.clientWidth > 0){
+        Indices_Block.style.display = "none";
         Info_Block.style.display = "block";
-    }
-
-    else if(Indice1_Block.clientWidth > 0){
-        Indice1_Block.style.display = "none";
-        Cb_Block.style.display = "block";
-    }
-
-    else if(Indice2_Block.clientWidth > 0){
-        Indice2_Block.style.display = "none";
-        Indice1_Block.style.display = "block";
     }
     
     else if(Players_Block.clientWidth > 0){
         Players_Block.style.display = "none";
-        Indice2_Block.style.display = "block";
+        Indices_Block.style.display = "block";
         Next_Button.style.display = "block";
     }
     
@@ -55,36 +42,24 @@ function Next_Action(){
     let Previous_Button = document.getElementById("previous");
     let Intro_Block = document.getElementById("intro");
     let Info_Block = document.getElementById("info");
-    let Cb_Block = document.getElementById("cb");
-    let Indice1_Block = document.getElementById("indice1");
-    let Indice2_Block = document.getElementById("indice2");
+    let Indices_Block = document.getElementById("indices");
 
     
     if(Intro_Block.clientWidth > 0){
-        console.log("coucou");
         Intro_Block.style.display = "none";
         Info_Block.style.display = "block";
         Previous_Button.style.display = "block";
+        Next_Button.textContent = ">";
     }
 
     else if(Info_Block.clientWidth > 0){
         Info_Block.style.display = "none";
-        Cb_Block.style.display = "block";
+        Indices_Block.style.display = "block";
     }
 
-    else if(Cb_Block.clientWidth > 0){
-        Cb_Block.style.display = "none";
-        Indice1_Block.style.display = "block";
-    }
-
-    else if(Indice1_Block.clientWidth > 0){
-        Indice1_Block.style.display = "none";
-        Indice2_Block.style.display = "block";
-    }
-
-    else if(Indice2_Block.clientWidth > 0){
-        Indice2_Block.style.display = "none";
-        Next_Button.style.display = "none";
+    else if(Indices_Block.clientWidth > 0){
+        Indices_Block.style.display = "none";
         Players_Block.style.display = "block";
-	}
+        Next_Button.style.display = "none";
+    }
 }
